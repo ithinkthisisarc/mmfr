c8a1566e2e9c52ef827469ee4de23fc2908413fc
Skye Bleed <developmentarc773@gmail.com>
2019-04-10 12:50:47 -0400

    Added support for multiple repositories

--- update.sh
+++ update.sh
@@ -1,3 +1,22 @@
+MAC=false
+LINUX=false
+
+for $i in $@; do
+  if [[ $i = "-l" ]]; then
+    LINUX=true
+  elif [[ $i = "-m" ]]; then
+    MAC=true
+  fi
+done
+
 echo "Updating package list..."
-curl "monarrksmysteriousfilerepo.glitch.me/links/names.info" > packages.sh
+curl "monarrksmysteriousfilerepo.glitch.me/links/sources.info" > sources.sh
+if [[ $MAC = true ]]; then
+  echo "Downloading sources for MAC"
+  curl "monarrksmysteriousfilerepo.glitch.me/links/mac.info" > mac.sh
+fi
+if [[ $LINUX = true ]]; then
+  echo "Downloading sources for LINUX"
+  curl "monarrksmysteriousfilerepo.glitch.me/links/linux.info" > linux.sh
+fi
 echo "OK"
